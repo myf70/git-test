409fe8764574c4516565b403879076447fcb00b7
MYF <mayunfei233@qq.com>
2022-04-23 21:32:20 +0800

    my first commit

---
+++ src/main/java/com/atguigu/git/Git.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author mayunfei
+ * @date 23/4/2022 下午9:27
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
